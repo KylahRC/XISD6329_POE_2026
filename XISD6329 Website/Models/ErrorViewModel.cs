2c21f76e806962e191052f4bc0b592f304a448df
Kylah Irvine <ST10435066@rcconnect.edu.za>
2026-08-31 09:41:11 +0200

    Add project files.

---
+++ XISD6329 Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XISD6329_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
