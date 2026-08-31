a9c241a5d73c4dc8f266cc0a995f39d26d752d90
Kylah Irvine <ST10435066@rcconnect.edu.za>
2026-08-31 10:34:48 +0200

    Added PagesController

---
+++ XISD6329 Website/Controllers/PagesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XISD6329_Website.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Accomodation()
+        {
+            return View();
+        }
+
+        public IActionResult BookingProcess()
+        {
+            return View();
+        }
+
+
+    }
+}
